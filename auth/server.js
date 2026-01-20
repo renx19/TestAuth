93697f393cfeadecc3894f96cef730abfb8cdc9f
Mjx17 <markjustin.borja17@gmail.com>
2026-01-20 14:15:04 +0800

    Update server.js

--- auth/server.js
+++ auth/server.js
@@ -36,7 +36,7 @@ app.use(session({
     httpOnly: true,
     secure: true,
     sameSite: 'none',
-    domain: '.example.com',
+    domain: '.onrender.com',
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }));
